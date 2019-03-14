17b9a1e53d4d636fb2d7bfc3fd92887e84f7c07e
Stefan Frei <stefan.a.frei@gmail.com>
2019-03-14 15:49:15 +0100

    improve test-case

--- imports/utils/validators.test.js
+++ imports/utils/validators.test.js
@@ -12,43 +12,31 @@ describe('URL validation', function() {
 	const isNotUrl = str => expect(IsUrl(str)).to.be.false;
 	const fails = notaString => expect(() => IsUrl(notaString)).to.throw();
 
-	it("accepts normal emails", function() {
-		isUrl("Jah7reix.poo0Ooz0@geUrl.com");
-		isUrl("poo0Ooz0@geemail.com");
-		isUrl("diFoh7ma@wooow.example");
+	it("accepts normal urls", function() {
+		isUrl("google.com");
+		isUrl("www.google.com");
+		isUrl("wooow.example");
 	});
 
 	it("accepts more url formats", function() {
-		isUrl("Jah7reix.poo0Ooz0@subdomain.geemail.com");
-		isUrl("J@s.g.c");
-		isUrl("0@sub.sub.sub.sub.sub");
-	});
-
-	it("accepts weird url formats", function() {
-		isUrl("Jah7reix+poo0Ooz0@geemail.com");
-		isUrl("Ó@£.cooooooom");
-		isUrl("-@ä.c");
-		isUrl("{_}@n-o.c");
+		isUrl("www.subdomain.geemail.com");
+		isUrl("www.s.g.c");
+		isUrl("www.sub.sub.sub.sub.sub");
 	});
 
 	it("rejects incomplete urls", function() {
 		// Yes it is technically possible to have an address @toplevel
 		// But we still assume it's an error.
-		isNotUrl("poo0Ooz0@geemail");
+		isNotUrl("geemail");
+		isNotUrl("cooooooom");
+		isNotUrl("example.");
+		isNotUrl(".example.");
 
-		isNotUrl("@£.cooooooom");
-		isNotUrl("-@");
-		isNotUrl("diFoh7ma@.");
-		isNotUrl("diFoh7ma@.example");
-		isNotUrl("diFoh7ma@example.");
 	});
 
 	it("rejects other strings", function() {
 		isNotUrl("Jah7reix.poo0Ooz0geemail.com");
-		isNotUrl("Jah7reix.poo0Ooz0");
-		isNotUrl("poo0Ooz0");
-		isNotUrl("é");
-		isNotUrl("0");
+		isNotUrl("abc/123.com")
 		isNotUrl("");
 	});
 
@@ -63,48 +51,23 @@ describe('URL validation', function() {
 });
 
 describe('Filetype validation', function() {
-	const isFiletype = str => expect(IsFiletype(str, ['png'])).to.be.true;
-	const isNotFiletype = str => expect(IsFiletype(str, ['png'])).to.be.false;
-	const fails = notaString => expect(() => IsFiletype(notaString, ['png'])).to.throw();
+	const isFiletype = str => expect(IsFiletype(str, ['png', 'pdf'])).to.be.true;
+	const isNotFiletype = str => expect(IsFiletype(str, ['png', 'pdf'])).to.be.false;
+	const fails = notaString => expect(() => IsFiletype(notaString, ['png', 'pdf'])).to.throw();
 
-	it("accepts normal emails", function() {
-		isFiletype("Jah7reix.poo0Ooz0@geFiletype.com");
-		isFiletype("poo0Ooz0@geemail.com");
-		isFiletype("diFoh7ma@wooow.example");
+	it("accepts filetypes", function() {
+		isFiletype("horse.png");
+		isFiletype("cat.png");
+		isFiletype("kakadu.png");
+		isFiletype('invoice.pdf');
 	});
 
-	it("accepts more url formats", function() {
-		isFiletype("Jah7reix.poo0Ooz0@subdomain.geemail.com");
-		isFiletype("J@s.g.c");
-		isFiletype("0@sub.sub.sub.sub.sub");
-	});
+	it("rejects filetypes", function() {
+		isNotFiletype("image.jpg");
+		isNotFiletype("image.jpeg");
+		isNotFiletype("image.gif");
+		isNotFiletype("audio.mp3");
 
-	it("accepts weird url formats", function() {
-		isFiletype("Jah7reix+poo0Ooz0@geemail.com");
-		isFiletype("Ó@£.cooooooom");
-		isFiletype("-@ä.c");
-		isFiletype("{_}@n-o.c");
-	});
-
-	it("rejects incomplete urls", function() {
-		// Yes it is technically possible to have an address @toplevel
-		// But we still assume it's an error.
-		isNotFiletype("poo0Ooz0@geemail");
-
-		isNotFiletype("@£.cooooooom");
-		isNotFiletype("-@");
-		isNotFiletype("diFoh7ma@.");
-		isNotFiletype("diFoh7ma@.example");
-		isNotFiletype("diFoh7ma@example.");
-	});
-
-	it("rejects other strings", function() {
-		isNotFiletype("Jah7reix.poo0Ooz0geemail.com");
-		isNotFiletype("Jah7reix.poo0Ooz0");
-		isNotFiletype("poo0Ooz0");
-		isNotFiletype("é");
-		isNotFiletype("0");
-		isNotFiletype("");
 	});
 
 	it("fails on non-strings", function() {
