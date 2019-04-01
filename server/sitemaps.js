70b61bcc811e18d419892ca2795c29b102edd767
Stefan Frei <stefan.a.frei@gmail.com>
2019-04-01 12:09:31 +0200

    use Router to generate url

--- server/sitemaps.js
+++ server/sitemaps.js
@@ -10,7 +10,7 @@ sitemaps.add('/sitemap.xml', function(req) {
 
 	courses.forEach((course) => {
 		out.push({
-			page: 'course/' + course._id + '/' + course.slug,
+			page: Router.url('showCourse', course),
 			lastmod: course.time_lastedit
 		});
 	});
