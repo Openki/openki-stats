81e52f6445266e72aa1f617b06a933288951e9ac
ToastHawaii <mar.pel@protonmail.com>
2021-01-07 11:39:40 +0100

    Add user search for admins

---
+++ imports/ui/pages/admin/users/users.js
@@ -0,0 +1,43 @@
+import { Meteor } from 'meteor/meteor';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Template } from 'meteor/templating';
+
+import UserSearchPrefix from '/imports/utils/user-search-prefix';
+
+import '/imports/ui/components/buttons/buttons';
+
+import './users.html';
+
+Template.users.onCreated(function () {
+	const instance = this;
+
+	instance.busy(false);
+
+	instance.userSearch = new ReactiveVar('');
+
+	instance.autorun(() => {
+		const search = instance.userSearch.get();
+		if (search.length > 0) {
+			Meteor.subscribe('userSearch', search);
+		}
+	});
+});
+
+Template.users.helpers({
+	foundUsers() {
+		const instance = Template.instance();
+
+		const search = instance.userSearch.get();
+		if (search === '') {
+			return false;
+		}
+
+		return UserSearchPrefix(search, { limit: 30 });
+	},
+});
+
+Template.users.events({
+	'keyup .js-search-users'(event, instance) {
+		instance.userSearch.set(instance.$('.js-search-users').val());
+	},
+});
