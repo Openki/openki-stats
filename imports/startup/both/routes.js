f51a578f9abf359eab52c01dede652cb0883fda8
ToastHawaii <mar.pel@protonmail.com>
2021-01-13 08:59:11 +0100

    improve docs

--- imports/startup/both/routes.js
+++ imports/startup/both/routes.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import { Session } from 'meteor/session'
+import { Session } from 'meteor/session';
 import moment from 'moment';
 import Courses from '/imports/api/courses/courses';
 import Events from '/imports/api/events/events';
