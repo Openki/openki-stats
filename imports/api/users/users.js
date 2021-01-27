f8046eb34f93ec5f389bbcf8cfb133f77fdbf0a9
ToastHawaii <mar.pel@protonmail.com>
2021-01-27 19:58:54 +0100

    Transform db model doc to jsdoc

--- imports/api/users/users.js
+++ imports/api/users/users.js
@@ -3,57 +3,63 @@ import { Meteor } from 'meteor/meteor';
 import IdTools from '/imports/utils/id-tools';
 
 // ======== DB-Model: ========
-// "_id"          -> ID
-// "createdAt"    -> Date
-// "services"     -> {
-//     password: {
-//         bcrypt:       String},
-//     github: {
-//         id:           Int32
-//         accessToken:  String
-//         email:        String/null
-//         username:     String }
-//     facebook: {
-//         accessTocken: String
-//         expiresAt:    Double
-//         id:           String
-//         email:        String       (not allways)
-//         name:         String
-//         first_name:   String
-//         last_name:    String
-//         link:         String
-//         gender:       String
-//         locale:       String }     ex: de_DE, en_US
-//     google: {
-//         accessTocken: String
-//         idTocken:     String
-//         expiresAt:    Double
-//         id:           String
-//         email:        String
-//         verified_email:Boolean
-//         name:         String
-//         given_name:   String
-//         family_name:  String
-//         picture:      String       (link)
-//         locale:       String }      ex: de
-//         scope:        [https://www.googleapis.com/auth/userinfo.email, https://www.googleapis.com/auth/userinfo.profile]
-//     resume: {
-//         loginTockens: [{when: Date, hashed: String}]}}
-// "username"     -> String
-// "emails"       -> [{address: String, verified: Boolean}]
-// "profile"      -> {name: String, regionId: ID}
-// "privileges"   -> [admin]
-// "lastLogin"    -> Date
-// "locale"       -> Lang             This value is managed by the messageformat package
-// notificactions: True if the user wants notification mails sent to them
+/**
+ * @typedef {Object} UserEntity
+ * @property {string} _id ID
+ * @property {Date} createdAt
+ * @property {object} services
+ * @property {object} services.password
+ * @property {string} services.password.bcrypt
+ * @property {object} services.github
+ * @property {number} services.github.id Int32
+ * @property {string} services.github.accessToken
+ * @property {string|null} services.github.email
+ * @property {string} services.github.username
+ * @property {object} services.facebook
+ * @property {string} services.facebook.accessTocken
+ * @property {number} services.facebook.expiresAt Double
+ * @property {string} services.facebook.id
+ * @property {string} [services.facebook.email] (not allways)
+ * @property {string} services.facebook.name
+ * @property {string} services.facebook.first_name
+ * @property {string} services.facebook.last_name
+ * @property {string} services.facebook.link
+ * @property {string} services.facebook.gender
+ * @property {string} services.facebook.locale  ex: de_DE, en_US
+ * @property {object} services.google
+ * @property {string} services.google.accessTocken
+ * @property {string} services.google.idTocken
+ * @property {number} services.google.expiresAt Double
+ * @property {string} services.google.id
+ * @property {string} services.google.email
+ * @property {boolean} services.google.verified_email
+ * @property {string} services.google.name
+ * @property {string} services.google.given_name
+ * @property {string} services.google.family_name
+ * @property {string} services.google.picture (link)
+ * @property {string} services.google.locale ex: de
+ * @property {string[]} services.google.scope [https://www.googleapis.com/auth/userinfo.email, https://www.googleapis.com/auth/userinfo.profile]
+ * @property {object} services.resume
+ * @property {{when: Date, hashed: string}[]} services.resume.loginTockens
+ * @property {string} username
+ * @property {{address: string, verified: Boolean}[]} emails
+ * @property {{name: string, regionId: string}} profile
+ * @property {string[]} privileges [admin]
+ * @property {Date} lastLogin
+ * @property {string} locale This value is managed by the messageformat package
+ * @property {boolean} notificactions True if the user wants notification mails sent to them
+ * @property {boolean} hidePricePolicy
+ * Calculated fields
+ * @property {string[]} badges union of user's id and group ids for permission checking,
+ * calculated by updateBadges()
+ * @property {string[]} groups List of groups the user is a member of, calculated by updateBadges()
+ * @property {boolean} acceptsMessages true if user has email address and the notifications
+ * flag is true. This is visible to other users.
+ */
 
-// Calculated fields
-// badges: union of user's id and group ids for permission checking, calculated by updateBadges()
-// groups: List of groups the user is a member of, calculated by updateBadges()
-// acceptsMessages: true if user has email address and the notifications flag is true.
-// This is visible to other users.
+/** @typedef {User & UserEntity} UserModel */
 
-export const User = function () {};
+export const User = function () { };
 
 export default Users = Meteor.users;
 
@@ -61,6 +67,7 @@ export default Users = Meteor.users;
   *
   * If the user is not logged-in, a placeholder "anon" object is
   * returned.
+  * @this {UserModel}
   */
 Users.currentUser = function () {
 	const logged = Meteor.user();
@@ -68,18 +75,18 @@ Users.currentUser = function () {
 		return logged;
 	}
 
+	/** @type {UserModel} */
 	const anon = new User();
 	anon._id = 'anon';
 	anon.anon = true;
 	return anon;
 };
 
-/** Check whether the user may promote things with the given group
-  *
-  * @param {String|Object} group - The group to check, this may be an Id or a group object
-  * @returns {Boolean}
-  *
+/** Check whether the user may promote things with the given group.
   * The user must be a member of the group to be allowed to promote things with it.
+  *
+  * @this {UserModel}
+  * @param {String|Object} group - The group to check, this may be an Id or a group object
   */
 User.prototype.mayPromoteWith = function (group) {
 	const groupId = IdTools.extract(group);
@@ -90,7 +97,8 @@ User.prototype.mayPromoteWith = function (group) {
 };
 
 /** Get email address of user
-  *
+ *
+  * @this {UserModel}
   * @returns String with email address or Boolean false
   */
 User.prototype.emailAddress = function () {
@@ -102,6 +110,7 @@ User.prototype.emailAddress = function () {
 
 /** Get verified email address of user
   *
+  * @this {UserModel}
   * @returns String with verified email address or Boolean false
   */
 User.prototype.verifiedEmailAddress = function () {
@@ -113,8 +122,8 @@ User.prototype.verifiedEmailAddress = function () {
 };
 
 /**
+ * @this {UserModel}
  * @param {string} role
- * @returns {boolean}
  */
 User.prototype.privileged = function (role) {
 	return this.privileges
