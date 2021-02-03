842935508cf8d9afc6afb3ea860cfc3c184cf258
ToastHawaii <mar.pel@protonmail.com>
2021-02-03 16:56:05 +0100

    Fix not updating user accepts messages flag

--- imports/startup/server/users.js
+++ imports/startup/server/users.js
@@ -8,6 +8,6 @@ Meteor.startup(() => {
 
 	Meteor.users.find({}, { fields: { notifications: 1, emails: 1 } }).observe({
 		added: Profile.updateAcceptsMessages,
-		updated: Profile.updateAcceptsMessages,
+		changed: Profile.updateAcceptsMessages,
 	});
 });
