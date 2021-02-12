ca6138e9c30e47b418f5d97eb3d9dc7bad088bdc
ToastHawaii <mar.pel@protonmail.com>
2021-02-12 15:00:00 +0100

    Remove show_subscribers from roles it is not used and not document

--- imports/api/roles/roles.js
+++ imports/api/roles/roles.js
@@ -4,7 +4,6 @@
  * @property {string}  type               (name of role)
  * @property {string}  icon               ex: "fa fa-bullhorn"
  * @property {boolean} [preset]          For always-on roles
- * @property {boolean} show_subscribers
  */
 
 /** @type {RoleEntity[]} */
@@ -13,23 +12,19 @@ const Roles = [
 		type: 'participant',
 		icon: 'fa fa-user',
 		preset: true,
-		show_subscribers: true,
 	},
 	{
 		type: 'mentor',
 		icon: 'fa fa-graduation-cap',
-		show_subscribers: true,
 	},
 	{
 		type: 'host',
 		icon: 'fa fa-home',
-		show_subscribers: true,
 	},
 	{
 		type: 'team',
 		icon: 'fa fa-bullhorn',
 		preset: true,
-		show_subscribers: true,
 	},
 ];
 
