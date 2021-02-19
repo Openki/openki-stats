1d064a02abdd0e724ae06e2b37c20e23a4c40323
ToastHawaii <mar.pel@protonmail.com>
2021-02-19 21:43:53 +0000

    Undo msgfmt test

--- imports/startup/both/msgfmt.js
+++ imports/startup/both/msgfmt.js
@@ -6,8 +6,6 @@ msgfmt.init('en', {
 	// Save setLocale() in Meteor.user().locale, sync to multiple clients (Default: true)
 	storeUserLocale: true,
 
-	waitOnLoaded: true,
-
 	// For safari older than v10 we need to connect to polyfill.io (Default: false)
 	disableIntlPolyfill: false,
 });
