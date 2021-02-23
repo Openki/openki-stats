ea50a9781fd3d497cdf6db80f8767dbc4d10bc46
ToastHawaii <mar.pel@protonmail.com>
2021-02-23 11:52:26 +0100

    Add hint for unexpected emails.

--- imports/utils/email-tools.js
+++ imports/utils/email-tools.js
@@ -12,6 +12,28 @@ const IsEmail = function (str) {
 export default IsEmail;
 
 /**
+ * provides sender and recipient email for report-email,defaults to
+ * sender reporter@mail.openki.net and recipient admins@openki.net
+ * if not set in configfile.
+ *
+ * @returns {{sender: string; recipient: string;}} - the desired sender and rcpt email
+ */
+export const getReportEmails = () => {
+	// preset, please override from configfile
+	const reportEmails = {
+		sender: 'reporter@mail.openki.net',
+		recipient: 'admins@openki.net',
+	};
+	if (Meteor.settings.reporter?.sender) {
+		reportEmails.sender = Meteor.settings.reporter.sender;
+	}
+	if (Meteor.settings.reporter?.recipient) {
+		reportEmails.recipient = Meteor.settings.reporter.recipient;
+	}
+	return reportEmails;
+};
+
+/**
   * Logo that can be attached to mails
   * @param {string} path a file path relative to private/
   */
