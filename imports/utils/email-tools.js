2d88aaf0839244c97a2051397dc0e0638b7dc972
ToastHawaii <mar.pel@protonmail.com>
2021-02-25 15:00:52 +0100

    make lint happy, some formatting

--- imports/utils/email-tools.js
+++ imports/utils/email-tools.js
@@ -1,9 +1,9 @@
 import { check } from 'meteor/check';
 
 /**
-  * Check a string if it is a valid email adress
-  * @param {string} str the string to be checked
-  */
+ * Check a string if it is a valid email adress
+ * @param {string} str the string to be checked
+ */
 const IsEmail = function (str) {
 	check(str, String);
 	return str.search(/^[^@\s]+@([^@.\s]+\.)+\w+$/g) === 0;
@@ -16,30 +16,17 @@ export default IsEmail;
  * sender reporter@mail.openki.net and recipient admins@openki.net
  * if not set in configfile.
  *
- * @returns {{sender: string; recipient: string;}} - the desired sender and rcpt email
+ * @returns the desired sender and rcpt email
  */
-export const getReportEmails = () => {
-	// preset, please override from configfile
-	const reportEmails = {
-		sender: 'reporter@mail.openki.net',
-		recipient: 'admins@openki.net',
-	};
-	if (Meteor.settings.reporter?.sender) {
-		reportEmails.sender = Meteor.settings.reporter.sender;
-	}
-	if (Meteor.settings.reporter?.recipient) {
-		reportEmails.recipient = Meteor.settings.reporter.recipient;
-	}
-	return reportEmails;
 export const getReportEmails = () => ({
 	sender: Meteor.settings.reporter?.sender || 'reporter@mail.openki.net',
-	recipient: Meteor.settings.reporter?.recipient || 'admins@openki.net'
-})
+	recipient: Meteor.settings.reporter?.recipient || 'admins@openki.net',
+});
 
 /**
-  * Logo that can be attached to mails
-  * @param {string} path a file path relative to private/
-  */
+ * Logo that can be attached to mails
+ * @param {string} path a file path relative to private/
+ */
 export const logo = function (path) {
 	check(path, String);
 	const cid = Random.id();
