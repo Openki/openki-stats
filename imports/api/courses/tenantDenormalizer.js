c173d035f2a857635ae1c9684e50dae611b5446b
ToastHawaii <mar.pel@protonmail.com>
2021-03-08 17:31:09 +0100

    Undo tenant filter on courses

---
+++ imports/api/courses/tenantDenormalizer.js
@@ -0,0 +1,21 @@
+import Regions from '/imports/api/regions/regions';
+/** @typedef {import('/imports/api/courses/courses').CourseModel} CourseModel */
+
+export default {
+	/**
+     * @param {CourseModel} course
+     */
+	enrich(course) {
+		if (!course.region) {
+			throw new Error('Unexpected falsy: course.region');
+		}
+
+		const region = Regions.findOne(course.region);
+
+		if (!region) {
+			throw new Error(`None matching region found for ${course.region}`);
+		}
+
+		return { ...course, tenant: region.tenant };
+	},
+};
