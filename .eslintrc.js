c3afbf91b2e91fea6634e934dcf59f44496d9333
ToastHawaii <mar.pel@protonmail.com>
2021-04-26 11:14:48 +0200

    Use stylelint for sass-linting, add prettier, add beautify, cleanup configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
 	env: {
 		browser: true,
-		es6: true,
+		es2021: true,
 		jquery: true,
 		meteor: true,
 		mocha: true,
 		node: true,
 	},
-	extends: [
-		'airbnb-base',
-	],
+	extends: ['airbnb-base', 'prettier'],
 	globals: {
 		L: 'readonly',
 		Log: 'writable',
@@ -19,23 +17,22 @@ module.exports = {
 		VERSION: 'writable',
 	},
 	parserOptions: {
-		ecmaVersion: 2020,
+		ecmaVersion: 12,
 		sourceType: 'module',
 	},
+	plugins: ['prettier'],
 	rules: {
 		'import/no-absolute-path': ['error', { esmodule: false }],
 		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-		indent: ['error', 'tab'],
-		'no-tabs': ['error', { allowIndentationTabs: true }],
 		'object-shorthand': ['error', 'always'],
 		'prefer-destructuring': ['error', { object: false, array: false }],
-		'no-multiple-empty-lines': ['error', { max: 2 }],
-		'no-extra-parens': ['error'],
 
 		// disabled rules
 		'func-names': [0],
 		'no-underscore-dangle': [0],
 		'max-classes-per-file': [0],
+
+		'prettier/prettier': 'error',
 	},
 	settings: {
 		'import/resolver': 'meteor',
