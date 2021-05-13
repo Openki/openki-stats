0e22f58bd04a9d24ac433409af1225e7979aac62
Lukas Burkhard <lks.burkhard@protonmail.ch>
2021-05-13 15:16:00 +0200

    Adds switch to disbale archived courses filter

--- imports/api/courses/courses.js
+++ imports/api/courses/courses.js
@@ -247,10 +247,12 @@ export class CoursesCollection extends Mongo.Collection {
 
 		const find = {};
 
-		if (!filter.archived) {
-			find.archived = { $ne: true }; // hide archived by default
-		} else {
-			find.archived = { $eq: true }; // only show archived
+		if (!filter.archivedDisabled) {
+			if (!filter.archived) {
+				find.archived = { $ne: true }; // hide archived by default
+			} else {
+				find.archived = { $eq: true }; // only show archived
+			}
 		}
 
 		if (filter.tenants && filter.tenants.length > 0) {
