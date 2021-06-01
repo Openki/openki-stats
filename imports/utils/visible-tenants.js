c86442632f12cd0caacc4fa5227714bf84712739
ToastHawaii <mar.pel@protonmail.com>
2021-06-01 15:20:15 +0200

    refactor visible tenants

--- imports/utils/visible-tenants.js
+++ imports/utils/visible-tenants.js
@@ -2,16 +2,16 @@ import { Meteor } from 'meteor/meteor';
 
 export function visibleTenants() {
 	/** @type {string[]} */
-	let usersTenants = [];
+	let usersTenants;
 
 	try {
 		// Source: https://forums.meteor.com/t/error-meteor-userid-can-only-be-invoked-in-method-calls-or-publications-in-collection-extends/44650/2
 		// Will throw an error unless within method call.
 		// Attempt to recover gracefully by catching:
-		usersTenants = Meteor.user().tenants || [];
-
-		// eslint-disable-next-line no-empty
-	} catch {}
+		usersTenants = Meteor.user()?.tenants || [];
+	} catch {
+		usersTenants = [];
+	}
 
 	return usersTenants.concat(Meteor.settings.public.publicTenants);
 }
