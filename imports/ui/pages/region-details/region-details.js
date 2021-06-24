0f3806cc383af36b6ef6c666fa45c7e9d2ffffb1
ToastHawaii <mar.pel@protonmail.com>
2021-06-24 15:32:45 +0200

    Display region details

--- imports/ui/pages/region-details/region-details.js
+++ imports/ui/pages/region-details/region-details.js
@@ -1,5 +1,54 @@
+import { Mongo } from 'meteor/mongo';
+import { Template } from 'meteor/templating';
+
+import { locationFormat } from '/imports/utils/location-format';
+
+import '/imports/ui/components/map/map';
+
 import './region-details.html';
 
 Template.regionDetails.onCreated(function () {
 	const instance = this;
+	instance.busy();
+
+	const markers = new Mongo.Collection(null); // Local collection for in-memory storage
+	instance.markers = markers;
+
+	/**
+	 * @param {{ coordinates: [number, number]; }} loc
+	 */
+	this.setLocation = (loc) => {
+		markers.remove({ main: true });
+		if (!loc) {
+			return;
+		}
+		markers.insert({
+			loc,
+			main: true,
+		});
+	};
+});
+
+Template.regionDetails.onRendered(function () {
+	const instance = this;
+
+	instance.busy(false);
+
+	instance.autorun(() => {
+		const data = Template.currentData();
+
+		instance.setLocation(data.region.loc);
+	});
+});
+
+Template.regionDetails.helpers({
+	markers() {
+		return Template.instance().markers;
+	},
+	/**
+	 * @param {{ coordinates: [number, number]; }} loc
+	 */
+	locationDisplay(loc) {
+		return locationFormat(loc);
+	},
 });
