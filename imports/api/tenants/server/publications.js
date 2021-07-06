b412c22c195d39188e4bd0441d14c0eff0da1164
ToastHawaii <mar.pel@protonmail.com>
2021-07-06 09:44:52 +0200

    Fix endless loading on logout by tenant

--- imports/api/tenants/server/publications.js
+++ imports/api/tenants/server/publications.js
@@ -3,20 +3,25 @@ import { Meteor } from 'meteor/meteor';
 import { Tenants } from '/imports/api/tenants/tenants';
 import * as UserPrivilegeUtils from '/imports/utils/user-privilege-utils';
 
-Meteor.publish('tenant', (tenantId) => {
+Meteor.publish('tenant', function (tenantId) {
+	const user = Meteor.user();
+
+	if (!user) {
+		this.ready();
+		return undefined;
+	}
+
 	const filter = { _id: tenantId };
 
 	// Only members of a tenant or admins can see a tenant
 	if (!UserPrivilegeUtils.privilegedTo('admin')) {
-		filter.members = Meteor.userId();
+		filter.members = user._id;
 	}
 
 	// Only admins can see all tenant admins. Note: Admin privileg is not something that is
 	// likely to happen and reactive changes are not needed.
 	const showAdminsFields =
-		UserPrivilegeUtils.privilegedTo('admin') || Meteor.user().isTenantAdmin(tenantId)
-			? 1
-			: undefined;
+		UserPrivilegeUtils.privilegedTo('admin') || user.isTenantAdmin(tenantId) ? 1 : undefined;
 
 	return Tenants.find(filter, { fields: { ...Tenants.publicFields, admins: showAdminsFields } });
 });
