32900573020e3a14e970d48e6437f14adb97aa48
ToastHawaii <mar.pel@protonmail.com>
2021-08-05 18:27:41 +0200

    Typescript migrate: some

--- imports/ui/lib/cleaned-region.ts
+++ imports/ui/lib/cleaned-region.ts
@@ -1,9 +1,7 @@
 /**
  * Use null instead of 'all' to mean "All regions".
  * This is needed until all instances where we deal with regions are patched.
- * @param {string} region
- * @returns {null | string}
  */
-export default function CleanedRegion(region) {
+export default function CleanedRegion(region: string) {
 	return region === 'all' ? null : region;
 }
