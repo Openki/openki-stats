c561b1f350a913cc073b259bb5339b852c682490
ToastHawaii <mar.pel@protonmail.com>
2021-08-16 16:59:24 +0200

    Add tests for user ahs contribtued

--- imports/ui/pages/userprofile/index.app-test.ts
+++ imports/ui/pages/userprofile/index.app-test.ts
@@ -13,7 +13,6 @@ if (Meteor.isClient) {
 		this.timeout(30000);
 		before(async function () {
 			this.timeout(8000);
-
 			Session.set('locale', 'en');
 			Session.set('region', 'all');
 		});
@@ -29,6 +28,82 @@ if (Meteor.isClient) {
 
 			await waitForSubscriptions();
 			await waitFor(haveTitle);
+		});
+
+		it('should allow admins to set "User has contributed"', async () => {
+			const buttonIsNotPresent = () => {
+				assert($('.js-has-contributed').length === 0, 'Button is not present');
+			};
+			const buttonIsPresent = () => {
+				assert($('.js-has-contributed').length > 0, 'Button is present');
+			};
+			const contributionIconIsPresent = () => {
+				assert(
+					$(
+						'a[data-tooltip="Sandro supported Hmmm with a donation. Click on the icon if you want to become a contributer as well."] i.fa.fa-heart',
+					).length > 0,
+					'Icon is present',
+				);
+			};
+
+			// A previous test might have logged us in.
+			if (Meteor.userId()) {
+				await MeteorAsync.logout();
+			}
+
+			(await MeteorAsync.subscribe('userSearch', 'Sandro')).stop(); // load user from server
+			const userId = Users.findOne({ username: 'Sandro' })._id;
+			Router.go('userprofile', { _id: userId, username: 'Sandro' });
+
+			await waitForSubscriptions();
+
+			await waitFor(buttonIsNotPresent);
+
+			await MeteorAsync.loginWithPassword('greg', 'greg');
+
+			await waitFor(buttonIsPresent);
+
+			$('.js-has-contributed').trigger('click');
+
+			await waitFor(contributionIconIsPresent);
+		});
+
+		it('should allow admins to unset "User has contributed"', async () => {
+			const buttonIsNotPresent = () => {
+				assert($('.js-unset-has-contributed').length === 0, 'Button is not present');
+			};
+			const buttonIsPresent = () => {
+				assert($('.js-unset-has-contributed').length > 0, 'Button is present');
+			};
+			const contributionIconIsNotPresent = () => {
+				assert(
+					$(
+						'a[data-tooltip="Sandro supported Hmmm with a donation. Click on the icon if you want to become a contributer as well."] i.fa.fa-heart',
+					).length === 0,
+					'Icon is not present',
+				);
+			};
+
+			// A previous test might have logged us in.
+			if (Meteor.userId()) {
+				await MeteorAsync.logout();
+			}
+
+			(await MeteorAsync.subscribe('userSearch', 'Sandro')).stop(); // load user from server
+			const userId = Users.findOne({ username: 'Sandro' })._id;
+			Router.go('userprofile', { _id: userId, username: 'Sandro' });
+
+			await waitForSubscriptions();
+
+			await waitFor(buttonIsNotPresent);
+
+			await MeteorAsync.loginWithPassword('greg', 'greg');
+
+			await waitFor(buttonIsPresent);
+
+			$('.js-unset-has-contributed').trigger('click');
+
+			await waitFor(contributionIconIsNotPresent);
 		});
 	});
 }
