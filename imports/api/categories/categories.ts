7360971bae321cef427a8da9a8f87522c89c5dce
ToastHawaii <mar.pel@protonmail.com>
2021-08-20 19:26:27 +0200

    Typescript: categories

--- imports/api/categories/categories.ts
+++ imports/api/categories/categories.ts
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
-const Categories = { ...Meteor.settings.public.categories };
+const Categories = { ...Meteor.settings.public.categories } as {
+	[category: string]: string[];
+};
 
 export default Categories;
