f68e77633b9308463162187c5cf428ec3c7fdc2c
ToastHawaii <mar.pel@protonmail.com>
2021-10-20 19:21:47 +0200

    Typescript & Bootstrap Migrate: log

--- imports/ui/pages/log/index.ts
+++ imports/ui/pages/log/index.ts
@@ -1,9 +1,10 @@
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Router } from 'meteor/iron:router';
-import { Template } from 'meteor/templating';
+import { Template as TemplateAny, TemplateStaticTyped } from 'meteor/templating';
 import { _ } from 'meteor/underscore';
 import moment from 'moment';
 
+import { LogCollection, LogEntity } from '/imports/api/log/factory';
 import { Log } from '/imports/api/log/log';
 
 import * as TemplateMixins from '/imports/ui/lib/template-mixins';
@@ -11,204 +12,218 @@ import RouterAutoscroll from '/imports/ui/lib/router-autoscroll';
 import * as UserPrivilegeUtils from '/imports/utils/user-privilege-utils';
 import * as UrlTools from '/imports/utils/url-tools';
 
-import './log.html';
+import './template.html';
+import './styles.scss';
 
-Template.logPage.onCreated(function () {
-	const instance = this;
-	const batchLoad = 100;
-	instance.updateUrl = () => {
-		const filterParams = instance.filter.toParams();
-		const queryString = UrlTools.paramsToQueryString(filterParams);
-
-		const options = {};
-
-		if (queryString.length) {
-			options.query = queryString;
+{
+	const Template = TemplateAny as TemplateStaticTyped<
+		'logPage',
+		// LogCollection.Filtering Params are allowed as data input
+		Parameters<ReturnType<LogCollection['Filtering']>['read']>[0],
+		{
+			filter: ReturnType<LogCollection['Filtering']>;
+			ready: ReactiveVar<boolean>;
+			limit: ReactiveVar<number>;
+			updateUrl: () => boolean;
 		}
+	>;
 
-		RouterAutoscroll.cancelNext();
+	const template = Template.logPage;
 
-		const router = Router.current();
-		Router.go(router.route.getName(), {}, options);
+	template.onCreated(function () {
+		const instance = this;
+		const batchLoad = 100;
+		instance.updateUrl = () => {
+			const filterParams = instance.filter.toParams();
+			const queryString = UrlTools.paramsToQueryString(filterParams);
 
-		return true;
-	};
+			const options: { query?: string } = {};
 
-	instance.ready = new ReactiveVar(false);
-	instance.limit = new ReactiveVar(batchLoad);
-
-	const filter = Log.Filtering();
-	instance.filter = filter;
-
-	// Read URL state
-	instance.autorun(() => {
-		const query = Template.currentData();
-		filter.clear().read(query).done();
-	});
-
-	// Update whenever filter changes
-	instance.autorun(() => {
-		const filterQuery = filter.toQuery();
-		instance.ready.set(false);
-
-		// Have some extra log entries ready so that they are shown immediately
-		// when more are demanded
-		const overLimit = instance.limit.get() + batchLoad + 1;
-		instance.subscribe('log', filterQuery, overLimit, () => {
-			instance.ready.set(true);
-		});
-	});
-});
-
-Template.logPage.helpers({
-	privileged() {
-		return UserPrivilegeUtils.privilegedTo('admin');
-	},
-
-	date() {
-		const start = Template.instance().filter.get('start');
-		return start?.toISOString() || '';
-	},
-
-	relFilter() {
-		const { rel } = Template.instance().filter.toParams();
-		return rel || '';
-	},
-
-	trFilter() {
-		const { tr } = Template.instance().filter.toParams();
-		return tr || '';
-	},
-
-	isodate(date) {
-		return moment(date).toISOString();
-	},
-
-	hasMore() {
-		const instance = Template.instance();
-
-		const filterQuery = instance.filter.toQuery();
-		const limit = instance.limit.get();
-		const results = Log.findFilter(filterQuery, limit + 1);
-
-		return results.count() > limit;
-	},
-
-	results() {
-		const instance = Template.instance();
-		const filterQuery = instance.filter.toQuery();
-		const entries = Log.findFilter(filterQuery, instance.limit.get()).fetch();
-		let last = false;
-		const inter = [];
-		entries.forEach((entry) => {
-			const ts = moment(entry.ts);
-			if (last) {
-				const interval = moment.duration(last.diff(ts));
-				if (interval.asMinutes() > 1) {
-					inter.push({ interval: interval.humanize() });
-				}
+			if (queryString.length) {
+				options.query = queryString;
 			}
-			inter.push(entry);
-			last = ts;
+
+			RouterAutoscroll.cancelNext();
+
+			const router = Router.current();
+			Router.go(router.route.getName(), {}, options);
+
+			return true;
+		};
+
+		instance.ready = new ReactiveVar(false);
+		instance.limit = new ReactiveVar(batchLoad);
+
+		const filter = Log.Filtering();
+		instance.filter = filter;
+
+		// Read URL state
+		instance.autorun(() => {
+			const query = Template.currentData();
+			filter.clear().read(query).done();
 		});
-		return inter;
-	},
 
-	loading() {
-		return !Template.instance().ready.get();
-	},
-});
+		// Update whenever filter changes
+		instance.autorun(() => {
+			const filterQuery = filter.toQuery();
+			instance.ready.set(false);
 
-Template.logPage.events({
-	// Update the URI when the search-field was changed an loses focus
-	'change .js-update-url'(_event, instance) {
-		instance.updateUrl();
-	},
+			// Have some extra log entries ready so that they are shown immediately
+			// when more are demanded
+			const overLimit = instance.limit.get() + batchLoad + 1;
+			instance.subscribe('log', filterQuery, overLimit, () => {
+				instance.ready.set(true);
+			});
+		});
+	});
 
-	'keyup .js-tr-input': _.debounce((_event, instance) => {
-		const { filter } = instance;
-		filter.disable('tr');
+	template.helpers({
+		privileged() {
+			return UserPrivilegeUtils.privilegedTo('admin');
+		},
 
-		const trStr = $('.js-tr-input').val().trim();
-		if (trStr) {
-			filter.add('tr', trStr);
-		}
+		date() {
+			const start = Template.instance().filter.get('start');
+			return start?.toISOString() || '';
+		},
 
-		filter.done();
-	}, 200),
+		relFilter() {
+			const { rel } = Template.instance().filter.toParams();
+			return rel || '';
+		},
 
-	'keyup .js-date-input': _.debounce((_event, instance) => {
-		const { filter } = instance;
-		const dateStr = $('.js-date-input').val().trim();
-		if (dateStr === '') {
-			filter.disable('start').done();
-		} else {
-			filter.add('start', dateStr).done();
-		}
-	}, 200),
+		trFilter() {
+			const { tr } = Template.instance().filter.toParams();
+			return tr || '';
+		},
 
-	'keyup .js-rel-input': _.debounce((_event, instance) => {
-		const { filter } = instance;
-		filter.disable('rel');
+		hasMore() {
+			const instance = Template.instance();
 
-		const relStr = $('.js-rel-input').val().trim();
-		if (relStr) {
-			filter.add('rel', relStr);
-		}
+			const filterQuery = instance.filter.toQuery();
+			const limit = instance.limit.get();
+			const results = Log.findFilter(filterQuery, limit + 1);
 
-		filter.done();
-	}, 200),
+			return results.count() > limit;
+		},
 
-	'click .js-tr'(event, instance) {
-		instance.filter.add('tr', `${this}`);
-		if (!event.shiftKey) {
-			instance.filter.done();
+		results() {
+			const instance = Template.instance();
+			const filterQuery = instance.filter.toQuery();
+			const entries = Log.findFilter(filterQuery, instance.limit.get()).fetch();
+			let last: moment.Moment | false = false;
+			const inter: (LogEntity | { interval: string })[] = [];
+			entries.forEach((entry) => {
+				const ts = moment(entry.ts);
+				if (last) {
+					const interval = moment.duration(last.diff(ts));
+					if (interval.asMinutes() > 1) {
+						inter.push({ interval: interval.humanize() });
+					}
+				}
+				inter.push(entry);
+				last = ts;
+			});
+			return inter;
+		},
+
+		loading() {
+			return !Template.instance().ready.get();
+		},
+	});
+
+	template.events({
+		// Update the URI when the search-field was changed an loses focus
+		'change .js-update-url'(_event, instance) {
 			instance.updateUrl();
-			window.scrollTo(0, 0);
-		}
-	},
+		},
 
-	'click .js-date'(event, instance) {
-		const start = moment(this).toISOString();
-		instance.filter.add('start', start);
-		if (!event.shiftKey) {
-			instance.filter.done();
-			instance.updateUrl();
-			window.scrollTo(0, 0);
-		}
-	},
+		'keyup .js-tr-input': _.debounce((_event, instance) => {
+			const { filter } = instance;
+			filter.disable('tr');
 
-	'click .js-rel-id'(event, instance) {
-		instance.filter.add('rel', `${this}`);
-		if (!event.shiftKey) {
-			instance.filter.done();
-			instance.updateUrl();
-			window.scrollTo(0, 0);
-		}
-	},
+			const trStr = ($('.js-tr-input').val() as string).trim();
+			if (trStr) {
+				filter.add('tr', trStr);
+			}
 
-	'click .js-more'(_event, instance) {
-		const { limit } = instance;
-		limit.set(limit.get() + 100);
-	},
-});
+			filter.done();
+		}, 200),
 
-TemplateMixins.MultiExpandible(Template, 'logEntry');
-Template.logEntry.helpers({
-	date() {
-		const { date } = Template.instance().filter.toParams();
-		return date?.toISOString() || '';
-	},
-	shortId(id) {
-		return id.substr(0, 8);
-	},
-	isodate(date) {
-		return moment(date).toISOString();
-	},
-	jsonBody() {
-		return JSON.stringify(this.body, null, '   ');
-	},
-	jsonFull() {
-		return JSON.stringify(this, null, '   ');
-	},
-});
+		'keyup .js-date-input': _.debounce((_event, instance) => {
+			const { filter } = instance;
+			const dateStr = ($('.js-date-input').val() as string).trim();
+			if (dateStr === '') {
+				filter.disable('start').done();
+			} else {
+				filter.add('start', dateStr).done();
+			}
+		}, 200),
+
+		'keyup .js-rel-input': _.debounce((_event, instance) => {
+			const { filter } = instance;
+			filter.disable('rel');
+
+			const relStr = ($('.js-rel-input').val() as string).trim();
+			if (relStr) {
+				filter.add('rel', relStr);
+			}
+
+			filter.done();
+		}, 200),
+
+		'click .js-tr'(event, instance) {
+			instance.filter.add('tr', `${this}`);
+			if (!(event as any).shiftKey) {
+				instance.filter.done();
+				instance.updateUrl();
+				window.scrollTo(0, 0);
+			}
+		},
+
+		'click .js-date'(event, instance) {
+			const start = moment(this).toISOString();
+			instance.filter.add('start', start);
+			if (!(event as any).shiftKey) {
+				instance.filter.done();
+				instance.updateUrl();
+				window.scrollTo(0, 0);
+			}
+		},
+
+		'click .js-rel-id'(event, instance) {
+			instance.filter.add('rel', `${this}`);
+			if (!(event as any).shiftKey) {
+				instance.filter.done();
+				instance.updateUrl();
+				window.scrollTo(0, 0);
+			}
+		},
+
+		'click .js-more'(_event, instance) {
+			const { limit } = instance;
+			limit.set(limit.get() + 100);
+		},
+	});
+}
+
+{
+	const Template = TemplateAny as TemplateStaticTyped<'logEntry'>;
+
+	const template = TemplateMixins.MultiExpandible(Template, 'logEntry');
+
+	template.helpers({
+		shortId(id: string) {
+			return id.substr(0, 8);
+		},
+		isodate(date: moment.Moment) {
+			return moment(date).toISOString();
+		},
+		jsonBody() {
+			return JSON.stringify(this.body, null, '   ');
+		},
+		jsonFull() {
+			return JSON.stringify(this, null, '   ');
+		},
+	});
+}
