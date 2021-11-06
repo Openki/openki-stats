e56e26307223ddd2d37f66e265dd514c5ad37ab0
ToastHawaii <mar.pel@protonmail.com>
2021-11-06 12:12:39 +0100

    Typescript & Bootstrap Migrate: splash

--- imports/ui/components/regions/splash/index.ts
+++ imports/ui/components/regions/splash/index.ts
@@ -1,5 +1,5 @@
 import { Session } from 'meteor/session';
-import { Template } from 'meteor/templating';
+import { Template as TemplateAny, TemplateStaticTyped } from 'meteor/templating';
 import $ from 'jquery';
 
 import '/imports/ui/components/regions/selection';
@@ -7,28 +7,32 @@ import '/imports/ui/components/regions/selection';
 import './template.html';
 import './styles.scss';
 
-Template.regionSplash.onRendered(function () {
+const Template = TemplateAny as TemplateStaticTyped<'regionSplash'>;
+
+const template = Template.regionSplash;
+
+template.onRendered(function () {
 	this.$('.js-region-splash').modal('show');
 });
 
-Template.regionSplash.events({
+template.events({
 	'hidden.bs.modal .js-region-splash'() {
 		Session.set('showRegionSplash', false);
 	},
 
-	'click .js-region-link'(event, instance) {
+	'click .js-region-link'(_event, instance) {
 		instance.$('.js-region-splash').modal('hide');
 	},
 
 	'click .js-region-search'(event, instance) {
-		instance.$(event.currentTarget).trigger('select');
+		instance.$(event.currentTarget as any).trigger('select');
 	},
 
-	'click .js-confirm-region'(event, instance) {
+	'click .js-confirm-region'(_event, instance) {
 		instance.$('.js-region-splash').modal('hide');
 	},
 
-	'click .js-login-for-region'(event, instance) {
+	'click .js-login-for-region'(_event, instance) {
 		$('.js-account-tasks').modal('show');
 		instance.$('.js-region-splash').modal('hide');
 	},
