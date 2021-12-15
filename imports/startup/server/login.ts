d66dc7fc49d9b6db212ade2b26d120caa837ec27
ToastHawaii <mar.pel@protonmail.com>
2021-12-15 22:06:42 +0100

    Typescript Migrate: server login

--- imports/startup/server/login.ts
+++ imports/startup/server/login.ts
@@ -2,23 +2,24 @@ import { Accounts } from 'meteor/accounts-base';
 import { Log } from '/imports/api/log/log';
 
 // Record a login attempt in the log
-const registerAttempt = (track) => (attempt) => {
-	const body = { ...attempt };
-	if (attempt.user) {
-		body.userId = attempt.user._id;
-		body.username = attempt.user.username;
-	}
+const registerAttempt =
+	(track: string) => (attempt: { user: { _id: string; username: string } }) => {
+		const body = { ...attempt } as any as { userId: string; username: string };
+		if (attempt.user) {
+			body.userId = attempt.user._id;
+			body.username = attempt.user.username;
+		}
 
-	// Too much (secret) info in the user object
-	delete body.user;
+		// Too much (secret) info in the user object
+		delete (body as any).user;
 
-	const rel = [];
-	if (body.userId) {
-		rel.push(body.userId);
-	}
+		const rel = [];
+		if (body.userId) {
+			rel.push(body.userId);
+		}
 
-	Log.record(track, rel, body);
-};
+		Log.record(track, rel, body);
+	};
 
 Accounts.onLogin(registerAttempt('Login.Success'));
 Accounts.onLoginFailure(registerAttempt('Login.Failure'));
