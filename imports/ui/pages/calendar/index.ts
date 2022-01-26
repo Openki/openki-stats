dde90bb19a0df0175eb35bb2e9d4a4ba6f93c08f
ToastHawaii <mar.pel@protonmail.com>
2022-01-26 19:20:16 +0100

    Typescript & Bootstrap Migrate: calendar

--- imports/ui/pages/calendar/index.ts
+++ imports/ui/pages/calendar/index.ts
@@ -3,208 +3,251 @@ import $ from 'jquery';
 import { i18n } from '/imports/startup/both/i18next';
 import { Session } from 'meteor/session';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { Template } from 'meteor/templating';
+import { Template as TemplateAny, TemplateStaticTyped } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
+import { Spacebars } from 'meteor/spacebars';
 import moment from 'moment';
 
-import { Events } from '/imports/api/events/events';
+import { Events, FindFilter } from '/imports/api/events/events';
 import * as UrlTools from '/imports/utils/url-tools';
 
 import '/imports/ui/components/events/list';
 import '/imports/ui/components/loading';
 
-import './calendar.html';
+import './template.html';
+import './styles.scss';
 
-Template.calendar.onCreated(function () {
-	const instance = this;
+{
+	const Template = TemplateAny as TemplateStaticTyped<
+		'calendar',
+		unknown,
+		{ filter: ReturnType<typeof Events['Filtering']>; eventSub: Meteor.SubscriptionHandle }
+	>;
 
-	const filter = Events.Filtering();
-	instance.filter = filter;
+	const template = Template.calendar;
 
-	// Read URL state
-	instance.autorun(() => {
-		const { query } = Router.current().params;
+	template.onCreated(function () {
+		const instance = this;
 
-		// Show internal events only when a group or venue is specified
-		if (!query.group && !query.venue && query.internal === undefined) {
-			query.internal = false;
-		}
+		const filter = Events.Filtering();
+		instance.filter = filter;
 
-		filter
-			.clear()
-			.add('start', moment().startOf('week'))
-			.read(query)
-			.add('region', Session.get('region'))
-			.done();
+		// Read URL state
+		instance.autorun(() => {
+			const { query } = Router.current().params;
+
+			// Show internal events only when a group or venue is specified
+			if (!query.group && !query.venue && query.internal === undefined) {
+				query.internal = false;
+			}
+
+			filter
+				.clear()
+				.add('start', moment().startOf('week').toISOString())
+				.read(query)
+				.add('region', Session.get('region'))
+				.done();
+		});
+
+		instance.autorun(() => {
+			const filterQuery = filter.toQuery() as FindFilter;
+
+			const start = filter.get('start').toDate();
+			const limit = filter.get('start').add(1, 'week').toDate();
+
+			filterQuery.period = [start, limit];
+			instance.eventSub = instance.subscribe('Events.findFilter', filterQuery);
+		});
 	});
 
-	instance.autorun(() => {
-		const filterQuery = filter.toQuery();
+	template.onRendered(function () {
+		const instance = Template.instance();
+		// change of week does not trigger onRendered again
+		this.autorun(() => {
+			// only do this in the current week
+			if (moment().format('w') === instance.filter.get('start').format('w')) {
+				if (instance.eventSub.ready()) {
+					Meteor.defer(function () {
+						const elem = instance.$('.js-calendar-date').eq(moment().weekday());
 
-		const start = filter.get('start').toDate();
-		const limit = filter.get('start').add(1, 'week').toDate();
-
-		if (moment().format('w') === moment(start).format('w')) {
-			instance.scrollNeeded = true;
-		}
-
-		filterQuery.period = [start, limit];
-		instance.eventSub = instance.subscribe('Events.findFilter', filterQuery);
+						// calendar nav and topnav are together 103 px fixed height, we add 7px margin
+						window.scrollTo(0, (elem.offset() || { top: 0 }).top - 110);
+					});
+				}
+			}
+		});
 	});
-});
 
-const updateUrl = function (event, instance) {
-	const filterParams = instance.filter.toParams();
-	delete filterParams.region; // HACK region is kept in the session (for bad reasons)
-	const queryString = UrlTools.paramsToQueryString(filterParams);
-
-	const options = {};
-	if (queryString.length) {
-		options.query = queryString;
-	}
-
-	Router.go('calendar', {}, options);
-	event.preventDefault();
-};
-
-Template.calendar.onRendered(function () {
-	// change of week does not trigger onRendered again
-	this.autorun(() => {
-		// only do this in the current week
-		if (moment().format('w') === Template.instance().filter.get('start').format('w')) {
-			const instance = Template.instance();
-			if (instance.eventSub.ready()) {
-				Meteor.defer(function () {
-					const elem = this.$('.js-calendar-date').eq(moment().weekday());
-					// calendar nav and topnav are together 103 px fixed height, we add 7px margin
-					window.scrollTo(0, elem.offset().top - 110);
+	template.helpers({
+		days() {
+			const start = Template.instance().filter.get('start');
+			let i = 0;
+			const days = [];
+			for (; i < 7; i += 1) {
+				days.push({
+					start: moment(start).add(i, 'days'),
+					end: moment(start).add(i + 1, 'days'),
 				});
 			}
-		}
+			return days;
+		},
+		filter() {
+			return Template.instance().filter;
+		},
+		startDate() {
+			return moment(Template.instance().filter.get('start'));
+		},
 	});
-});
-
-Template.calendar.helpers({
-	days() {
-		const start = Template.instance().filter.get('start');
-		let i = 0;
-		const days = [];
-		for (; i < 7; i += 1) {
-			days.push({
-				start: moment(start).add(i, 'days'),
-				end: moment(start).add(i + 1, 'days'),
-			});
+}
+{
+	const Template = TemplateAny as TemplateStaticTyped<
+		'calendarDayFormat',
+		{
+			day: {
+				start: moment.Moment;
+				end: moment.Moment;
+			};
+			filter: ReturnType<typeof Events['Filtering']>;
 		}
-		return days;
-	},
-	filter() {
-		return Template.instance().filter;
-	},
-	startDate() {
-		return moment(Template.instance().filter.get('start'));
-	},
-});
+	>;
 
-Template.calendarDayFormat.helpers({
-	hasEvents() {
-		const filterQuery = this.filter.toQuery();
-		filterQuery.period = [this.day.start.toDate(), this.day.end.toDate()];
+	const template = Template.calendarDayFormat;
 
-		return Events.findFilter(filterQuery, 1).count() > 0;
-	},
-	events() {
-		const filterQuery = this.filter.toQuery();
-		filterQuery.period = [this.day.start.toDate(), this.day.end.toDate()];
+	template.helpers({
+		hasEvents() {
+			const data = Template.currentData();
+			const filterQuery = data.filter.toQuery() as FindFilter;
+			filterQuery.period = [data.day.start.toDate(), data.day.end.toDate()];
 
-		return Events.findFilter(filterQuery);
-	},
-	eventsReady() {
-		const instance = Template.instance();
-		return instance.parentInstance().eventSub.ready();
-	},
-});
+			return Events.findFilter(filterQuery, 1).count() > 0;
+		},
+		events() {
+			const data = Template.currentData();
+			const filterQuery = data.filter.toQuery() as FindFilter;
+			filterQuery.period = [data.day.start.toDate(), data.day.end.toDate()];
 
-Template.calendarNav.helpers({
-	endDateTo(date) {
-		return moment(date).add(6, 'days');
-	},
-});
-
-Template.calendarNav.onCreated(function () {
-	this.currentUnit = new ReactiveVar('week');
-});
-
-Template.calendarNav.onRendered(function () {
-	const navContainer = this.$('.calendar-nav-container');
-	navContainer.slideDown();
-
-	$(window).on('scroll', () => {
-		const isCovering = navContainer.hasClass('calendar-nav-container-covering');
-		const atTop = $(window).scrollTop() < 5;
-
-		if (!isCovering && !atTop) {
-			navContainer.addClass('calendar-nav-container-covering');
-		} else if (isCovering && atTop) {
-			navContainer.removeClass('calendar-nav-container-covering');
-		}
+			return Events.findFilter(filterQuery);
+		},
+		eventsReady() {
+			const instance = Template.instance();
+			return (instance.parentInstance() as any).eventSub.ready();
+		},
 	});
-});
+}
+{
+	const Template = TemplateAny as TemplateStaticTyped<
+		'calendarNav',
+		unknown,
+		{ currentUnit: ReactiveVar<string> }
+	>;
 
-const mvDateHandler = function (unit, instance) {
-	const amount = instance.data.direction === 'previous' ? -1 : 1;
-	const calendarInstance = instance.parentInstance(2);
-	const start = calendarInstance.filter.get('start');
-	const weekCorrection = unit === 'week' ? 0 : 1;
+	const template = Template.calendarNav;
 
-	if (amount < 0) {
-		start.add(amount, unit).startOf('week');
-	} else {
-		start.add(amount, unit).add(weekCorrection, 'week').startOf('week');
-	}
-	calendarInstance.filter.add('start', start).done();
-	/* eslint-disable-next-line no-restricted-globals */
-	updateUrl(event, calendarInstance);
-	return false;
-};
+	template.helpers({
+		endDateTo(date: moment.Moment) {
+			return moment(date).add(6, 'days');
+		},
+	});
 
-Template.calendarNavControl.events({
-	'click .js-change-date'(event, instance) {
-		const unit = instance.parentInstance().currentUnit.get();
-		mvDateHandler(unit, instance);
-	},
+	template.onCreated(function () {
+		this.currentUnit = new ReactiveVar('week');
+	});
 
-	'click .js-change-unit'(event, instance) {
-		const unit = this;
-		instance.parentInstance().currentUnit.set(unit);
-		mvDateHandler(unit, instance);
-	},
-});
+	template.onRendered(function () {
+		const navContainer = this.$('.calendar-nav-container');
+		navContainer.slideDown();
 
-Template.calendarNavControl.helpers({
-	arrow() {
-		let isRTL = Session.equals('textDirectionality', 'rtl');
+		$(window).on('scroll', () => {
+			const isCovering = navContainer.hasClass('calendar-nav-container-covering');
+			const atTop = ($(window).scrollTop() || 0) < 5;
 
-		if (this.direction === 'previous') {
-			isRTL = !isRTL;
+			if (!isCovering && !atTop) {
+				navContainer.addClass('calendar-nav-container-covering');
+			} else if (isCovering && atTop) {
+				navContainer.removeClass('calendar-nav-container-covering');
+			}
+		});
+	});
+}
+
+{
+	const updateUrl = function (instance: any) {
+		const filterParams = instance.filter.toParams();
+		delete filterParams.region; // HACK region is kept in the session (for bad reasons)
+		const queryString = UrlTools.paramsToQueryString(filterParams);
+
+		const options: { query?: string } = {};
+		if (queryString.length) {
+			options.query = queryString;
 		}
 
-		const direction = isRTL ? 'left' : 'right';
-		return Spacebars.SafeString(
-			`<span class="fa fa-arrow-${direction} fa-fw" aria-hidden="true"></span>`,
-		);
-	},
+		Router.go('calendar', {}, options);
+	};
+	const mvDateHandler = function (unit: string, instance: any) {
+		const amount = instance.data.direction === 'previous' ? -1 : 1;
+		const calendarInstance = instance.parentInstance(2);
+		const start = calendarInstance.filter.get('start');
+		const weekCorrection = unit === 'week' ? 0 : 1;
 
-	calendarNavText(direction, unit, length) {
-		return i18n(`calendar.${direction}.${unit}.${length}`);
-	},
+		if (amount < 0) {
+			start.add(amount, unit).startOf('week');
+		} else {
+			start.add(amount, unit).add(weekCorrection, 'week').startOf('week');
+		}
+		calendarInstance.filter.add('start', start).done();
+		/* eslint-disable-next-line no-restricted-globals */
+		updateUrl(calendarInstance);
+		return false;
+	};
 
-	currentUnit() {
-		const parentInstance = Template.instance().parentInstance();
-		return parentInstance.currentUnit.get();
-	},
+	const Template = TemplateAny as TemplateStaticTyped<'calendarNavControl', { direction: string }>;
 
-	navUnits() {
-		const navUnits = ['week', 'month', 'year'];
-		return navUnits;
-	},
-});
+	const template = Template.calendarNavControl;
+
+	template.events({
+		'click .js-change-date'(event, instance) {
+			event.preventDefault();
+			const unit = (instance.parentInstance() as any).currentUnit.get();
+			mvDateHandler(unit, instance);
+		},
+
+		'click .js-change-unit'(this: string, event, instance) {
+			event.preventDefault();
+
+			const unit = this;
+			(instance.parentInstance() as any).currentUnit.set(unit);
+			mvDateHandler(unit, instance);
+		},
+	});
+
+	template.helpers({
+		arrow() {
+			const data = Template.currentData();
+
+			let isRTL = Session.equals('textDirectionality', 'rtl');
+
+			if (data.direction === 'previous') {
+				isRTL = !isRTL;
+			}
+
+			const direction = isRTL ? 'left' : 'right';
+			return Spacebars.SafeString(
+				`<span class="fa fa-arrow-${direction} fa-fw" aria-hidden="true"></span>`,
+			);
+		},
+
+		calendarNavText(direction: string, unit: string, length: string) {
+			return i18n(`calendar.${direction}.${unit}.${length}`);
+		},
+
+		currentUnit() {
+			const parentInstance = Template.instance().parentInstance() as any;
+			return parentInstance.currentUnit.get();
+		},
+
+		navUnits() {
+			const navUnits = ['week', 'month', 'year'];
+			return navUnits;
+		},
+	});
+}
