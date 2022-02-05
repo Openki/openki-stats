98ecebbb8ea56087cad5682da7e598a78e5dc229
ToastHawaii <mar.pel@protonmail.com>
2022-02-05 09:16:02 +0100

    Typescript & Bootstrap Migrate: delete events

--- imports/ui/components/courses/delete-events/index.ts
+++ imports/ui/components/courses/delete-events/index.ts
@@ -1,176 +1,203 @@
 import { i18n } from '/imports/startup/both/i18next';
 import { ReactiveDict } from 'meteor/reactive-dict';
-import { Template } from 'meteor/templating';
+import { Template as TemplateAny, TemplateStaticTyped } from 'meteor/templating';
 import moment from 'moment';
 
 import { Regions } from '/imports/api/regions/regions';
 import * as Alert from '/imports/api/alerts/alert';
 import * as EventsMethods from '/imports/api/events/methods';
+import { EventEntity, EventModel } from '/imports/api/events/events';
 
 import { Analytics } from '/imports/ui/lib/analytics';
 
 import './template.html';
 import './styles.scss';
 
-Template.deleteCourseEvents.events({
-	'mouseover/mouseout .js-show-events-delete-modal'(event, instance) {
-		// mouseover/mouseout doesn't get caught on child elements. so we get it
-		const caption = instance.$(event.currentTarget).children('.event-caption-action');
-		caption.toggleClass('placeholder', event.type === 'mouseout');
-	},
+{
+	const Template = TemplateAny as TemplateStaticTyped<'deleteCourseEvents'>;
 
-	'click .js-show-events-delete-modal'(event, instance) {
-		instance.parentInstance().showModal.set(true);
-	},
-});
+	const template = Template.deleteCourseEvents;
 
-Template.deleteEventsModal.onCreated(function () {
-	this.busy(false);
+	template.events({
+		'mouseover/mouseout .js-show-events-delete-modal'(event, instance) {
+			// mouseover/mouseout doesn't get caught on child elements. so we get it
+			const caption = instance.$(event.currentTarget as any).children('.event-caption-action');
+			caption.toggleClass('placeholder', event.type === 'mouseout');
+		},
 
-	this.state = new ReactiveDict();
-	this.state.setDefault({
-		selectedEvents: [],
-		allEventsSelected: false,
-		showDeleteConfirm: false,
+		'click .js-show-events-delete-modal'(_event, instance) {
+			(instance.parentInstance() as any).showModal.set(true);
+		},
 	});
+}
 
-	// set allEventsSelected to true if all events are selected
-	this.autorun(() => {
-		const cursor = Template.currentData().upcomingEvents;
-		const allEventsSelected = cursor.count() === this.state.get('selectedEvents').length;
-		this.state.set({ allEventsSelected });
-	});
-
-	// close confirmation dialog if no events are selected
-	this.autorun(() => {
-		if (this.state.get('showDeleteConfirm')) {
-			if (!this.state.get('selectedEvents').length) {
-				this.state.set('showDeleteConfirm', false);
-			}
+{
+	const Template = TemplateAny as TemplateStaticTyped<
+		'deleteEventsModal',
+		{ upcomingEvents: Mongo.Cursor<EventEntity, EventModel> },
+		{
+			state: ReactiveDict<{
+				selectedEvents: EventModel[];
+				allEventsSelected: boolean;
+				showDeleteConfirm: boolean;
+			}>;
 		}
-	});
-});
+	>;
 
-Template.deleteEventsModal.onRendered(function () {
-	this.$('.js-delete-events-modal').modal('show');
-});
+	const template = Template.deleteEventsModal;
 
-Template.deleteEventsModal.helpers({
-	selectedEvents() {
-		return Template.instance().state.get('selectedEvents');
-	},
+	template.onCreated(function () {
+		this.busy(false);
 
-	isSelected() {
-		return Template.instance()
-			.state.get('selectedEvents')
-			.find((e) => e._id === this._id);
-	},
-
-	numSelectedEvents() {
-		return Template.instance().state.get('selectedEvents').length;
-	},
-
-	disabledIfNoEventsSelected() {
-		if (Template.instance().state.get('selectedEvents').length === 0) {
-			return 'disabled';
-		}
-		return '';
-	},
-});
-
-Template.deleteEventsModal.events({
-	'hidden.bs.modal .js-delete-events-modal'(event, instance) {
-		instance.parentInstance().showModal.set(false);
-	},
-
-	'click .js-toggle-all'(event, instance) {
-		let selectedEvents;
-		if (instance.state.get('allEventsSelected')) {
-			selectedEvents = [];
-		} else {
-			selectedEvents = Template.currentData().upcomingEvents.fetch();
-		}
-
-		instance.state.set({ selectedEvents });
-	},
-
-	'change input[type="checkbox"]'(event, instance) {
-		let selectedEvents = instance.state.get('selectedEvents');
-		if (event.target.checked) {
-			selectedEvents.push(this);
-		} else {
-			selectedEvents = selectedEvents.filter((e) => e._id !== this._id);
-		}
-
-		instance.state.set({ selectedEvents });
-	},
-
-	'click .js-show-delete-confirm'(event, instance) {
-		instance.state.set('showDeleteConfirm', true);
-	},
-
-	'click .js-deselect-event'(e, instance) {
-		const eventId = instance.$(e.target).data('event-id');
-		const selectedEvents = instance.state.get('selectedEvents');
-		instance.state.set(
-			'selectedEvents',
-			selectedEvents.filter((event) => event._id !== eventId),
-		);
-	},
-
-	'click .js-delete-events'(e, instance) {
-		instance.busy('deleting');
-
-		const events = instance.state.get('selectedEvents');
-		let removed = 0;
-		let responses = 0;
-		events.forEach((event) => {
-			EventsMethods.remove(event._id)
-				.then(() => {
-					removed += 1;
-				})
-				.catch((err) => {
-					const start = moment(event.startLocal).format('llll');
-					Alert.serverError(
-						err,
-						i18n(
-							'deleteEventsModal.errWithReason',
-							'Could not delete the "{TITLE} ({START})" event.',
-							{ TITLE: event.title, START: start },
-						),
-					);
-				})
-				.finally(() => {
-					responses += 1;
-					if (responses === events.length) {
-						instance.busy(false);
-						instance.state.set('showDeleteConfirm', false);
-						if (removed) {
-							Alert.success(
-								i18n(
-									'deleteEventsModal.sucess',
-									'{NUM, plural, one{Event was} other{# events were} } successfully deleted.',
-									{ NUM: removed },
-								),
-							);
-
-							Analytics.trackEvent(
-								'Events deletions',
-								'Events deletions as team',
-								Regions.findOne(event.region)?.nameEn,
-								removed,
-							);
-						}
-						if (removed === responses) {
-							instance.state.set('selectedEvents', []);
-							instance.$('.js-delete-events-modal').modal('hide');
-						}
-					}
-				});
+		this.state = new ReactiveDict();
+		this.state.setDefault({
+			selectedEvents: [],
+			allEventsSelected: false,
+			showDeleteConfirm: false,
 		});
-	},
 
-	'click .js-cancel'(event, instance) {
-		instance.state.set('showDeleteConfirm', false);
-	},
-});
+		// set allEventsSelected to true if all events are selected
+		this.autorun(() => {
+			const cursor = Template.currentData().upcomingEvents;
+			const allEventsSelected = cursor.count() === this.state.get('selectedEvents')?.length;
+			this.state.set({ allEventsSelected });
+		});
+
+		// close confirmation dialog if no events are selected
+		this.autorun(() => {
+			if (this.state.get('showDeleteConfirm')) {
+				if (!this.state.get('selectedEvents')?.length) {
+					this.state.set('showDeleteConfirm', false);
+				}
+			}
+		});
+	});
+
+	template.onRendered(function () {
+		this.$('.js-delete-events-modal').modal('show');
+	});
+
+	template.helpers({
+		isSelected(this: EventModel) {
+			return Template.instance()
+				.state.get('selectedEvents')
+				?.find((e) => e._id === this._id);
+		},
+
+		numSelectedEvents() {
+			return Template.instance().state.get('selectedEvents')?.length || 0;
+		},
+
+		disabledIfNoEventsSelected() {
+			if (Template.instance().state.get('selectedEvents')?.length === 0) {
+				return 'disabled';
+			}
+			return '';
+		},
+	});
+
+	template.events({
+		'hidden.bs.modal .js-delete-events-modal'(_event, instance) {
+			(instance.parentInstance() as any).showModal.set(false);
+		},
+
+		'click .js-toggle-all'(_event, instance) {
+			let selectedEvents: EventModel[];
+			if (instance.state.get('allEventsSelected')) {
+				selectedEvents = [];
+			} else {
+				selectedEvents = Template.currentData().upcomingEvents.fetch();
+			}
+
+			instance.state.set({ selectedEvents });
+		},
+
+		'change input[type="checkbox"]'(this: EventModel, event, instance) {
+			let selectedEvents = instance.state.get('selectedEvents');
+
+			if (!selectedEvents) {
+				throw new Error('Unexpected falsy: selectedEvents');
+			}
+
+			if ((event.target as HTMLInputElement).checked) {
+				selectedEvents.push(this);
+			} else {
+				selectedEvents = selectedEvents.filter((e) => e._id !== this._id);
+			}
+
+			instance.state.set({ selectedEvents });
+		},
+
+		'click .js-show-delete-confirm'(_event, instance) {
+			instance.state.set('showDeleteConfirm', true);
+		},
+
+		'click .js-deselect-event'(e, instance) {
+			const eventId = instance.$(e.target as any).data('event-id');
+			const selectedEvents = instance.state.get('selectedEvents');
+			if (!selectedEvents) {
+				throw new Error('Unexpected falsy: selectedEvents');
+			}
+			instance.state.set(
+				'selectedEvents',
+				selectedEvents.filter((event) => event._id !== eventId),
+			);
+		},
+
+		'click .js-delete-events'(_e, instance) {
+			instance.busy('deleting');
+
+			const events = instance.state.get('selectedEvents');
+			let removed = 0;
+			let responses = 0;
+			events?.forEach((event) => {
+				EventsMethods.remove(event._id)
+					.then(() => {
+						removed += 1;
+					})
+					.catch((err) => {
+						const start = moment(event.startLocal).format('llll');
+						Alert.serverError(
+							err,
+							i18n(
+								'deleteEventsModal.errWithReason',
+								'Could not delete the "{TITLE} ({START})" event.',
+								{ TITLE: event.title, START: start },
+							),
+						);
+					})
+					.finally(() => {
+						responses += 1;
+						if (responses === events.length) {
+							instance.busy(false);
+							instance.state.set('showDeleteConfirm', false);
+							if (removed) {
+								Alert.success(
+									i18n(
+										'deleteEventsModal.sucess',
+										'{NUM, plural, one{Event was} other{# events were} } successfully deleted.',
+										{ NUM: removed },
+									),
+								);
+
+								Analytics.trackEvent(
+									'Events deletions',
+									'Events deletions as team',
+									Regions.findOne(event.region)?.nameEn,
+									removed,
+								);
+							}
+							if (removed === responses) {
+								instance.state.set('selectedEvents', []);
+								instance.$('.js-delete-events-modal').modal('hide');
+							}
+						}
+					});
+			});
+		},
+
+		'click .js-cancel'(_event, instance) {
+			instance.state.set('showDeleteConfirm', false);
+		},
+	});
+}
