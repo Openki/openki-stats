5f08544774067ce1a50df8ff293fad1df58e0244
ToastHawaii <mar.pel@protonmail.com>
2022-03-06 20:53:13 +0100

    Typescript & Bootstrap Migrate: frame body

--- imports/ui/layouts/frame-body.ts
+++ imports/ui/layouts/frame-body.ts
@@ -1,14 +1,18 @@
-import { Template } from 'meteor/templating';
+import { Template as TemplateAny, TemplateStaticTyped } from 'meteor/templating';
 
 import '/imports/ui/components/alerts';
 
 import './frame-body.html';
 
-/* Workaround to prevent iron-router from messing with server-side downloads
- *
- * Class 'js-download' must be added to those links.
- */
-Template.frameLayout.events({
+const Template = TemplateAny as TemplateStaticTyped<'frameLayout'>;
+
+const template = Template.frameLayout;
+
+template.events({
+	/* Workaround to prevent iron-router from messing with server-side downloads
+	 *
+	 * Class 'js-download' must be added to those links.
+	 */
 	'click .js-download'(event) {
 		event.stopPropagation();
 	},
